cc9f982502cf6413bd2ed2c79387d70599ffdbd2
Luciano Marin <mescalkun@gmail.com>
2023-03-28 11:51:52 -0300

    error

--- resources/views/dashboard/error/errorAC.blade.php
+++ resources/views/dashboard/error/errorAC.blade.php
@@ -5,25 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error</title>
+    <link href="css/estilo.css" rel="stylesheet" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
 
 </head>
 <body>
-    
+<div class="contenedor">
 <div class="container-fluid">
     <div class="row">
-    <div class="col-md-6 border">
-    
+    <div class="col-md-6 IPError">
+    <div class="backgrounderror">
     <br>
-    <h1>No tiene los permisos para acceder a esta pagina</h1>
+    <div class="tError">    
+ 
+    <label>No tiene los permisos para acceder a esta pagina!</label>
+    </div>
+
 </div>
-<div class="col-md-6 border">
-    <label>Inicio</label>
+</div>
+<div class="col-md-6 border pError">
+    <label class="textoPrincipal">Para acceder a estas funciones debe configurar su perfil de especialista.</label>
+    <br>
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+    <a href="" class="linksError">Inicio</a>
+</div>
+<div class="col-md-4">
+   <a href="" class="linksError"> Perfil</a>
+</div>
+<div class="col-md-4">
+   <a href="" class="linksError"> Otro</a>
 </div>
 </div>
 </div>
 
+</div>
+</div>
 
+
+</div>
 
 
 </body>
